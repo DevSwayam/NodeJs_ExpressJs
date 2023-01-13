9db77360d2bacd0925240ea4d61ea2019c0c5f02
Swayam rajkumar karle <99035115+DevSwayam@users.noreply.github.com>
2023-01-14 01:22:07 +0530

    Blocking and synchronous code

--- streamBuffer.js
+++ streamBuffer.js
@@ -21,12 +21,16 @@ const app = http.createServer((req,res)=>{
             body.push(chunk);
         });
 
+        /* If you notice we have written return in front of req.on() function cause 
+        we want our node js stop and return this response cause node js is async then he wont wait
+        for this event to get execute and if he finds something below this function which responds to
+        client side then he will return that response which is wrong reffer to 1.20.00 */
         //Agar aur data aya to buffer me store hote hey wo usko kese access kare wo ye below function batata hey
-        req.on("end",()=>{ // jese hi end wala function run hua hum
+        return req.on("end",()=>{ // jese hi end wala function run hua hum
             const parseBody = Buffer.concat(body).toString();// buffer me jo bhi data hey use concat karo 
             const message = parseBody.split('=')[1]; // hum msg=swayam hu bhai nahi chahiye isliye "=" ke pehle wala split karke text file me store karenge
             // console.log(parseBody); // print kardo
-            fs.writeFileSync("stremFile.txt",message);
+            fs.writeFileSync("stremFile.txt",message); // writeFileSunc() bata raha ki ye function ka pura wait karo jab tak ye write na kare text file me (synchronous behaviour) called as blocking code
         });
         fs.writeFileSync("NewFile.txt","Helloooo"); 
         res.statusCode=302; 
